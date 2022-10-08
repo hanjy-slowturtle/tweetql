60b34ad52de0a8863a3fa053ce5ad05cd144b7fc
hanjy-slowturtle <h920915@gmail.com>
2022-10-09 00:24:37 +0900

    ~ #4.4 Non Nullable Fields

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    username: String!
+    firstName: String!
+    lastName: String!
   }
   type Tweet {
     id: ID!
@@ -13,7 +15,7 @@ const typeDefs = gql`
 
   type Query {
     allTweets: [Tweet!]!
-    tweet(id: ID!): Tweet!
+    tweet(id: ID!): Tweet
   }
 
   type Mutation {
